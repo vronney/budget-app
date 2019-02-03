8b1c43d60c6466442dc9a1bdcb22fa4cc9a4733c
Ronney Vargas <ronneyvargas@gmail.com>
2019-02-03 16:32:37 -0600

    Created eventListener method to hold eventListeners and created init function to initiate when app opens.

--- app.js
+++ app.js
@@ -35,8 +35,18 @@ var UIController = (function() {
  *********************************/
 var controller = (function (budgetCtrl, UICtrl) {
     
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
 
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) { // '.which' is used for older browser instead of '.keyCode'
+                ctrlAddItem();
+            }
+        });
+    };
+    
     var ctrlAddItem = function() {
         // 1. Get the field input data 
         var input = UICtrl.getInput();
@@ -45,18 +55,18 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) { // '.which' is used for older browser instead of '.keyCode'
-            ctrlAddItem();   
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
+    };
+
 })(budgetController, UIController);
 
-
+controller.init();
 
 
 
