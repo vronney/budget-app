7a461a291cd89984323f5e6905da6cda8f793374
Ronney Vargas <ronneyvargas@gmail.com>
2019-02-03 16:47:30 -0600

    Created contructors for expenses and income.  Created methods to hold exp and inc data.

--- app.js
+++ app.js
@@ -3,7 +3,30 @@
 /************ Budget Module ********
  *********************************/
 var budgetController = (function () {
-    // Some code
+    
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
 })();
 
 /************ UI Module **********
@@ -50,7 +73,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
         // 1. Get the field input data 
         var input = UICtrl.getInput();
-        console.log(input);
+        
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
