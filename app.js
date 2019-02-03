6ee58d1bab1b8583bb850f2b224ea3c94811d37d
Ronney Vargas <ronneyvargas@gmail.com>
2019-02-03 17:22:02 -0600

    Created data method.

--- app.js
+++ app.js
@@ -25,7 +25,33 @@ var budgetController = (function () {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            // create new ID 
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+               ID = 0; 
+            }
+            
+            // create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+                } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+                }
+                // push it into data structure
+                data.allItems[type].push(newItem);
+                // return the new element
+                return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -71,10 +97,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
     
     var ctrlAddItem = function() {
+        var input, newItem;
+        
         // 1. Get the field input data 
         var input = UICtrl.getInput();
-        
         // 2. Add the item to the budget controller
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
